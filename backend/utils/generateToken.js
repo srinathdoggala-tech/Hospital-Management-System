ba386aa5e2e2a9db932bb643c7e2e60134570eca
srinathdoggala-tech <doggalasrinath@gmail.com>
2026-06-13 11:37:02 +0530

    Add JWT_SECRET environment variable and code fallback to prevent jsonwebtoken sign crash

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 exports.generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'secret_key_for_hospital_management_system', {
         expiresIn: '30d',
     })
 }
